9e76be8e696fadb480505bd13ad0e672c467875c
vinoth <vinu.barro@gmail.com>
2020-08-01 06:38:56 -0400

    init

---
+++ Project1/condor.cmd
@@ -0,0 +1,11 @@
+Executable = ./run.sh
+Universe = vanilla
+getenv = true
+error = project1_vinoth.error
+Log = project1_vinoth.log
+#arguments =
+output = project1_vinoth.out
+transfer_executable = false
+request_memory = 1024
+notify_user = vinoth@uw.edu
+Queue
