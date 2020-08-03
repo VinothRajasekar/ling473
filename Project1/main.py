92e0dd88d521413b2ec2b66d7adf9a525237931c
vinoth <vinu.barro@gmail.com>
2020-08-03 01:03:37 -0400

    init

--- Project1/main.py
+++ Project1/main.py
@@ -1,86 +1,80 @@
-import numpy as np
 import glob
 from typing import List
 from collections import defaultdict
-import logging
+from nltk.tree import ParentedTree
+from nltk.tokenize import SExprTokenizer
+from nltk.tgrep import tgrep_nodes
 
 
-def flatten_tree(parse_tree) -> List:
-    cleaned_data = parse_tree.split()
-    return cleaned_data
+class Constituents(object):
 
+    def __int__(self):
+        pass
 
-def count_sentence(data) -> int:
-    print(data)
-    sentence = '(S'
-    sentence_count = data.count(sentence)
-    print(sentence_count)
-    return sentence_count
+    def process_data(self, data) -> List:
+        cleaned_data = ''.join(map(str.strip, data))
+        tokenizer = SExprTokenizer()
+        tokens = tokenizer.tokenize(cleaned_data)
+        processed_data = [ParentedTree.fromstring(value) for value in tokens]
+        return processed_data
 
+    def calculate_count(self, tree_filtered) -> int:
+        total_length = sum(len(row) for row in tree_filtered)
+        return total_length
 
-def count_np(data) -> int:
-    noun_phrase = '(NP'
-    np_count = data.count(noun_phrase)
-    print(np_count)
-    return np_count
+    def count_sentence(self, data) -> int:
+        tree_filtered = list(tgrep_nodes('S', data))
+        return self.calculate_count(tree_filtered)
 
+    def count_np(self, data) -> int:
+        tree_filtered = list(tgrep_nodes('NP', data))
+        return self.calculate_count(tree_filtered)
 
-def count_vp(data) -> int:
-    verb_phrase = '(VP'
-    vp_count = data.count(verb_phrase)
-    print(vp_count)
-    return vp_count
+    def count_vp(self, data) -> int:
+        tree_filtered = list(tgrep_nodes('VP', data))
+        return self.calculate_count(tree_filtered)
 
+    def ditransitive_count(self, data) -> int:
+        count = 0
+        for tree in data:
+            for subtree in tree.subtrees(lambda t: len(t) == 3):
+                tree_filtered = list(tgrep_nodes("VP < (NP $ NP)", subtree))
+                count += self.calculate_count(tree_filtered)
+        return count
 
-def ditransitive_count(data) -> int:
-    count = 0
-    for i, word in enumerate(data):
-        try:
-            if word == '(VP' and data[i + 2] == '(NP' and data[i + 3] == '(NP' and data[i + 4] != '(':
+    def intransitive_count(self, data) -> int:
+
+        count = 0
+        for tree in data:
+            for _ in tree.subtrees(lambda t: t.label() == "VP" and len(t) == 1):
                 count += 1
-        except IndexError as e:
-            logging.exception("Index Error", e)
+        return count
 
-    return count
+    def main(self) -> None:
+        print("Starting to Process data")
+        results = defaultdict(list)
+        filepath = "/Users/vinoth/PycharmProjects/ling473/test.prd"
+        #filepath = "/corpora/LDC/LDC99T42/RAW/parsed/prd/wsj/14/*.prd"
+        filenames = glob.glob(filepath)
 
+        for file in filenames:
+            with open(file) as f:
+                parse_trees = f.readlines()
+                parsed_data = self.process_data(parse_trees)
+                results['S'].append(self.count_sentence(parsed_data))
+                results['NP'].append(self.count_np(parsed_data))
+                results['VP'].append(self.count_vp(parsed_data))
+                results['DVP'].append(self.ditransitive_count(parsed_data))
+                results['IVP'].append(self.intransitive_count(parsed_data))
 
-def intransitive_count(data) -> int:
-    data_len = len(data) - 1
-    count = 0
-    for i, word in enumerate(data):
-        if i + 2 < data_len and i + 3 < data_len:
-            try:
-                if word == '(VP' and (data[i + 2] == '(S' or data[i + 2] == '(.'):
-                    count += 1
-            except IndexError as e:
-                logging.exception("Index Error", e)
+        results_count = {}
 
-    return count
-
-
-def main() -> None:
-    print("project 1 work")
-    results = defaultdict(list)
-    print(results)
-    filepath = "/Users/vinoth/PycharmProjects/ling473/*.prd"
-    filenames = glob.glob(filepath)
-
-    for file in filenames:
-        with open(file) as f:
-            parse_tree = f.read()
-            data = flatten_tree(parse_tree)
-            results['S'].append(count_sentence(data))
-            results['NP'].append(count_np(data))
-            results['VP'].append(count_vp(data))
-            results['DVP'].append(ditransitive_count(data))
-            results['IVP'].append(intransitive_count(data))
-
-    print(results)
-    results_count = {}
-    for key, value in results.items():
-        results_count[key] = sum(value)
-    print(results_count)
+        for key, value in results.items():
+            results_count[key] = sum(value)
+            print(f"The constituent count for {key} is {results_count[key]}")
+        print("Completed Processing data successfully")
 
 
-if __name__ == main():
-    main()
+if __name__ == "__main__":
+    process = Constituents()
+    process.main()
